41b658ddf0a106589c685e533795be7194da11da
Ossian Winter <ossian@hey.com>
2023-07-16 21:18:05 +0200

    Allow extending scanner system

---
+++ src/main/java/enterprises/iridian/di/scan/SimpleFieldScanner.java
@@ -0,0 +1,29 @@
+package enterprises.iridian.di.scan;
+
+import javax.inject.Inject;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SimpleFieldScanner implements FieldScanner {
+
+  @Override
+  public List<Field> scan(final Class<?> typeClass) {
+    final List<Field> fields = new ArrayList<>();
+
+    for (final Field field : typeClass.getDeclaredFields()) {
+      if (!field.isAnnotationPresent(Inject.class)) {
+        continue;
+      }
+
+      if (Modifier.isFinal(field.getModifiers())) {
+        continue;
+      }
+
+      fields.add(field);
+    }
+
+    return fields;
+  }
+}
